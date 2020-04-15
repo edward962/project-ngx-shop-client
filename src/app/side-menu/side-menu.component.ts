08d115ce97d328106d1a9d206630fb91b81495f5
AndrewDavydenko <drun2098@gmail.com>
2020-04-15 19:24:14 +0300

    feature: side-menu[ref:no-ref]

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.sass']
+  styleUrls: ['./side-menu.component.sass'],
 })
 export class SideMenuComponent implements OnInit {
+  public show: string;
+  public currentIndex: number| null = null;
+  public cats = [
+    { name: 'Camera & Photo', subCategories: [{ name: '1' }, { name: '2' }] },
+    { name: 'Home Cinema, TV & Video', subCategories: [{ name: '1' }, { name: '2' }] },
+    { name: 'Mobile Phones', subCategories: [{ name: '1' }, { name: '2' }] },
+    { name: 'Computers & Components', subCategories: [{ name: '1' }, { name: '2' }] },
+  ];
 
-  constructor() { }
+  hover(index: number) {
+    this.currentIndex = index;
+  }
+  unHover() {
+  this.currentIndex = null;
+  }
+  constructor() {}
 
-  ngOnInit(): void {
- }
-
+  ngOnInit(): void {}
 }
