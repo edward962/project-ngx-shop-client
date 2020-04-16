fc6cd6d29f94416dd478f0e4c4d511198874cd10
svetlana-b <svetlana.btw93@gmail.com>
2020-04-16 19:40:52 +0300

    Feature: create slider for main page [ref: no-ref]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './header/header.component';
 import { CardComponent } from './card/card.component';
 import { StarRatingComponent } from './card/star-rating/star-rating.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CarouselDirective } from './carousel/carousel.directive';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { SideMenuComponent } from './side-menu/side-menu.component';
     CardComponent,
     StarRatingComponent,
     SideMenuComponent,
+    CarouselComponent,
+    CarouselDirective,
   ],
   imports: [BrowserModule, AppRoutingModule, MatIconModule],
   providers: [],
---
+++ src/app/carousel/carousel.directive.ts
@@ -0,0 +1,76 @@
+import {
+  Directive,
+  EmbeddedViewRef,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
+
+@Directive({
+  selector: '[ngShopCarousel]',
+})
+export class CarouselDirective implements OnInit {
+  @Input()
+  public appExchangeRates: any;
+  // tslint:disable-next-line: no-input-rename
+  @Input('ngShopCarouselFrom')
+  public images: { src: string; title: string }[] = [];
+  // tslint:disable-next-line: no-input-rename
+  @Input('ngShopCarouselDelay')
+  public ms: number = 5000;
+  @Input('ngShopCarouselAutoplay')
+  public set playAuto(mode: 'on' | 'off') {
+    if (!mode) {
+      return;
+    }
+    this.autoplay = mode;
+  }
+
+  public autoplay: 'on' | 'off' = 'on';
+  public currentView!: EmbeddedViewRef<any>;
+  public context: any;
+  public index: number = 0;
+  private intervalID!: any;
+
+  public constructor(
+    private readonly tpl: TemplateRef<any>,
+    private readonly vcr: ViewContainerRef
+  ) {}
+
+  public ngOnInit(): void {
+    this.context = {
+      $implicit: this.images[this.index],
+      controller: {
+        start: () => this.start(),
+        stop: () => this.stop(),
+      },
+    };
+    this.currentView = this.vcr.createEmbeddedView(this.tpl, this.context);
+    this.start();
+  }
+
+  public next(): void {
+    this.index++;
+    if (this.index >= this.images.length) {
+      this.index = 0;
+    }
+    this.currentView.destroy();
+    this.context = {
+      ...this.context,
+      $implicit: this.images[this.index],
+    };
+    this.currentView = this.vcr.createEmbeddedView(this.tpl, this.context);
+  }
+
+  public start(): void {
+    this.intervalID = setInterval(() => {
+      this.next();
+    }, this.ms);
+  }
+
+  public stop(): CarouselDirective {
+    clearInterval(this.intervalID);
+    return this;
+  }
+}
