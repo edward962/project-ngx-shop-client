f705f2fef6a24cd83c6e18dfbe4f6ea70a29ccff
AndrewDavydenko <drun2098@gmail.com>
2020-04-16 15:40:55 +0300

    feature: side-menu mocked[ref:no-ref]

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  // tslint:disable-next-line: component-selector
-  selector: 'app-header',
+  selector: 'app-ngx-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass'],
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() {}
-
-  ngOnInit(): void {
-  }
+export class HeaderComponent {
 }
--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -1,27 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ICategory } from './interfaces/category.interface';
+import { categories } from '../data/mock-data';
 
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.sass'],
 })
-export class SideMenuComponent implements OnInit {
+export class SideMenuComponent {
   public show: string;
   public currentIndex: number| null = null;
-  public cats = [
-    { name: 'Camera & Photo', subCategories: [{ name: '1' }, { name: '2' }] },
-    { name: 'Home Cinema, TV & Video', subCategories: [{ name: '1' }, { name: '2' }] },
-    { name: 'Mobile Phones', subCategories: [{ name: '1' }, { name: '2' }] },
-    { name: 'Computers & Components', subCategories: [{ name: '1' }, { name: '2' }] },
-  ];
-
+  public categories: ICategory[] = categories;
   hover(index: number) {
     this.currentIndex = index;
   }
   unHover() {
   this.currentIndex = null;
   }
-  constructor() {}
-
-  ngOnInit(): void {}
 }
