ab97a7125aac7bd7dbd7cb3ad4ca06f6c808bd11
svetlana-b <svetlana.btw93@gmail.com>
2020-04-16 17:39:58 +0300

    Feature: side-menu component imported[ref: no-ref]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { CardComponent } from './card/card.component';
 import { PricePipe } from './pipes/price.pipe';
 import { StarRatingComponent } from './card/star-rating/star-rating.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { StarRatingComponent } from './card/star-rating/star-rating.component';
     CardComponent,
     PricePipe,
     StarRatingComponent,
+    SideMenuComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, MatIconModule],
   providers: [],
--- src/app/pipes/price.pipe.ts
+++ src/app/pipes/price.pipe.ts
@@ -8,8 +8,9 @@ export class PricePipe implements PipeTransform {
   constructor(private currencyPipe: CurrencyPipe) {}
 
   transform(value: any, currency: string, symbol: boolean = false): string {
-    if (value != null)
+    if (value != null) {
       return this.currencyPipe.transform(value, currency, symbol);
+    }
     return this.currencyPipe.transform(0, currency, symbol).split('0.00')[0];
   }
 }
