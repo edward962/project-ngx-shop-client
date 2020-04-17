3c82948f72b6e8873eda7af4d3f4ee1573e0054a
AndrewDavydenko <drun2098@gmail.com>
2020-04-17 16:29:55 +0300

    feature: routing(main-cart) [ref:no-ref]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent,
-  children: [
   {
     path: '',
     loadChildren: () => import('./content/main-content/main-content.module').then(mod => mod.MainContentModule),
@@ -15,8 +10,6 @@ const routes: Routes = [
     path: 'cart',
     loadChildren: () => import('./content/cart/cart.module').then(mod => mod.CartModule),
   },
-  ]
-}
 ];
 
 @NgModule({
