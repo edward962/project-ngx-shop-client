1517a7785176368158323998e78e1bbdd3409696
AndrewDavydenko <drun2098@gmail.com>
2020-04-23 16:12:30 +0300

    feature: one-product [ref:no-ref]

--- src/app/content/main-content/product-list/one-product/one-product.component.ts
+++ src/app/content/main-content/product-list/one-product/one-product.component.ts
@@ -6,9 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./one-product.component.sass']
 })
 export class OneProductComponent implements OnInit {
-
+  public currentIndex: number | null = null;
+  public isShow = false;
+  public chars = [
+  '1Beginner\'s DSLR to tell the story as you see it',
+  '24.2 MP, APS-C sensor, 5 fps, Dual Pixel CMOS AF',
+  '3Wi-Fi, NFC, Bluetooth, vari-angle touch screen LCD',
+  '4Beginner\'s DSLR to tell the story as you see it',
+  '524.2 MP, APS-C sensor, 5 fps, Dual Pixel CMOS AF',
+  '6Wi-Fi, NFC, Bluetooth, vari-angle touch screen LCD',
+  '7Beginner\'s DSLR to tell the story as you see it',
+  '824.2 MP, APS-C sensor, 5 fps, Dual Pixel CMOS AF',
+  '9Wi-Fi, NFC, Bluetooth, vari-angle touch screen LCD'
+  ];
+  show(){
+    this.isShow = !this.isShow;
+  }
   constructor() { }
-
   ngOnInit(): void {
   }
 
