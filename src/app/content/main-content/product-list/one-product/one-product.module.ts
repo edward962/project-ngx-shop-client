5576be74cd506fdeae7c7b06d232b820380e46d3
IronMan756 <andrewstarudubtsev@gmail.com>
2020-04-23 18:43:02 +0300

    fix [ref:no-ref]

--- src/app/content/main-content/product-list/one-product/one-product.module.ts
+++ src/app/content/main-content/product-list/one-product/one-product.module.ts
@@ -6,7 +6,7 @@ import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [OneProductComponent],
+  // declarations: [OneProductComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
