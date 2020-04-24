aa6719c46ffb43df2e78f506c89383ce7bf1989b
AndrewDavydenko <drun2098@gmail.com>
2020-04-24 18:08:47 +0300

    feature: animation&footer-fix [ref:no-ref]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, Data } from '@angular/router';
+import { customTransition } from './router.animation';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
+  animations: [customTransition]
 })
 
 export class AppComponent{
   title = 'ngx-shop-client';
+  public getRouterState(outlet: RouterOutlet): Data {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.state;
+  }
 }
