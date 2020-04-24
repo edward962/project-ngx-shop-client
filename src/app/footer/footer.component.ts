b166ce9f601dc5b3f711604caebb4844b94b7f8f
svetlana-b <svetlana.btw93@gmail.com>
2020-04-24 11:12:23 +0300

    Feature: footer added [ref: no-ref]

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ngx-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.sass'],
+})
+export class FooterComponent {}
