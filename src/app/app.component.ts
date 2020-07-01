edb281726d005987dc78f1a70ce3a9cc3c8bab5e
Andrew <andrewstarodubtsev@Andrews-iMac.local>
2020-07-01 19:53:24 +0300

    Feature: current solution[ref:no-ref]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, Data } from '@angular/router';
 import { fadeAnimation } from './router.animation';
-import { trigger, transition } from '@angular/animations';
 
 @Component({
   selector: 'ngx-shop-root',
   templateUrl: './app.component.html',
-  animations: [	trigger(
-    'blockInitialRenderAnimation',
-    [
-      transition( ':enter', [] ),
-    ]
-  ), fadeAnimation],
+  animations: [fadeAnimation],
 })
 export class AppComponent {
-  public hideFooter = true;
   public getRouterState(outlet: RouterOutlet): Data {
     return (
-      outlet && outlet.activatedRouteData && outlet.activatedRouteData.state
+       outlet && outlet.activatedRouteData && outlet.activatedRouteData.state
     );
   }
   public animationStart(): void {
-    this.hideFooter = true;
   }
 
   public animationEnd(): void {
-    this.hideFooter = false;
   }
+
 }
